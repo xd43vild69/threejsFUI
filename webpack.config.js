05eef7ea924a60eb5425cc289579ce28d44c05ef
xd43vild69 <xd43vild69@gmail.com>
2021-06-04 16:42:51 -0500

    webpack test1
    basic project js

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: { index: path.resolve(__dirname, "src", "index.js") },
+  output: {
+    path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+  ]
+};
