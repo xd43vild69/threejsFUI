11b31e51513b17118bdd6e1a93fc857a3e8d76b7
xd43vild69 <xd43vild69@gmail.com>
2021-06-04 19:39:43 -0500

    Add three dependency example.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 import {upperCase} from 'lodash'
+import * as THREE from 'three';
+
 console.log("hello dev");
 console.log(upperCase("thisIsANewTestToCallLodash")); 
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+
+const sizeBox = 0.5;
+const cubeGeometry = new THREE.BoxGeometry(sizeBox, sizeBox, sizeBox)
+const cubeMaterial = new THREE.MeshBasicMaterial({
+    color: '#ff0000'
+})
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+scene.add(cubeMesh)
+
+// Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
